d8f8f09eb2cb2eaf4deaef42cf66e266ab409345
Davide Pugliese <deviad@gmail.com>
2020-05-07 00:50:10 +0300

    First commit

---
+++ src/main/java/org/example/MultiThreadedBLockingServer.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+@Slf4j
+public class MultiThreadedBLockingServer {
+    @SneakyThrows
+    public static void main(String[] args) {
+        ServerSocket ss = new ServerSocket(8080);
+        while (true) {
+            Socket s = ss.accept();
+            new Thread(() -> SocketHandler.HANDLE.accept(s, SocketHandler::transmogrify)).start();
+        }
+
+    }
+
+}
---
+++ src/main/java/org/example/SocketHandler.java
@@ -0,0 +1,35 @@
+package org.example;
+
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UncheckedIOException;
+import java.net.Socket;
+import java.util.function.BiConsumer;
+import java.util.function.UnaryOperator;
+
+@Slf4j
+@UtilityClass
+public class SocketHandler {
+    public BiConsumer<Socket, UnaryOperator<Integer>> HANDLE = (s, transmogrify)-> {
+        try (s; InputStream in = s.getInputStream(); OutputStream out = s.getOutputStream()) {
+            int data;
+            in.transferTo(out);
+            while ((data = in.read()) != -1) {
+                out.write(transmogrify.apply(data));
+            }
+        } catch (IOException ex) {
+            log.info(ex.getMessage());
+            throw new UncheckedIOException(ex);
+        }
+    };
+
+    public int transmogrify(int data) {
+        return Character.isLetter(data) ? data ^ ' ': data;
+    }
+
+}
